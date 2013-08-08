30a27db2731b0fb43d4077b5dac86f438ec9ac99
nattaporn <gogorenger@gmail.com>
2013-08-09 00:56:48 +0700

    change extends to SimpleSwingApplication

--- src/main/scala/QuickHealDesktop.scala
+++ src/main/scala/QuickHealDesktop.scala
@@ -1,6 +1,6 @@
 import scala.swing._
 
-object QuickHealDesktop extends SimpleGUIApplication {
+object QuickHealDesktop extends SimpleSwingApplication {
 	def top = new MainFrame {
 		title = "Quick Heal"
 		val button = new Button {
