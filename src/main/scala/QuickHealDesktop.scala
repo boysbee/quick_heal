e9dff677edca461886956162c3be8a727c4b19e4
nattaporn <gogorenger@gmail.com>
2013-08-09 02:01:07 +0700

    add search panel and result panel

--- src/main/scala/QuickHealDesktop.scala
+++ src/main/scala/QuickHealDesktop.scala
@@ -1,18 +1,48 @@
 import scala.swing._
 
 object QuickHealDesktop extends SimpleSwingApplication {
+	setSystemLookAndFeel()
+	// search panel select
+	val searchLabel = new Label {
+		text = "Select a search criteria :"
+	}
+	// search text field
+	var searchTextField = new TextField {
+		columns = 10
+	}
+	// search criteria combo box
+	val searchSelect = new ComboBox(List("Discount Code","Job Name"))
+	// find button
+	val searchButton = new Button {
+		text = "Find"
+	}
+	// search panel
+	var searchPanel = new FlowPanel {
+		contents += searchLabel
+		contents += searchTextField
+		contents += searchSelect
+		contents += searchButton
+
+	}
+	// result panel
+	var resultPanel = new BoxPanel(Orientation.Vertical) {
+		
+		contents += new ScrollPane(new Table())
+	}
+
 	def top = new MainFrame {
 		title = "Quick Heal"
-		val button = new Button {
-			text = "Find"
-		}
-		val label = new Label {
-			text = "No button clicks registered"
-		}	
+		
+			
 		contents = new BoxPanel(Orientation.Vertical) {
-			contents += button
-			contents += label
-			border = Swing.EmptyBorder(30, 30, 10, 30)
+			contents += searchPanel
+			contents += resultPanel
+			border = Swing.EmptyBorder(0, 0, 0, 0)
 		}
-	}	
+	}
+
+	def setSystemLookAndFeel() {
+		import javax.swing.UIManager
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
+  }	
 }
