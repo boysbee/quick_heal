c9c16880e8f9ab949e304d75dca54545ee625198
nattaporn <gogorenger@gmail.com>
2013-08-09 00:40:05 +0700

    add spec search with discount code

--- src/test/scala/QuickHealSpec.scala
+++ src/test/scala/QuickHealSpec.scala
@@ -1,11 +1,24 @@
 import org.scalatest._
+import org.scalatest.matchers._
 
-class QuickHealSpec extends FreeSpec with GivenWhenThen  {
+class QuickHealSpec extends FreeSpec with GivenWhenThen with ShouldMatchers {
 
+	val fixure = 
+		new {
+			val quickHeal = new QuickHeal()
+		}
 
-	"A QuickHeal Swing" - {
-		"Can check discount code is contains in job" in {
+	"A QuickHeal " - {
 
+		"Should return result list of job name" in {
+			val quickHeal = fixure.quickHeal
+			given("discount code")
+			var discountCode = "DIR040"
+			when("use findJobListByDiscountCode")
+			var result = quickHeal.findJobListByDiscountCode(discountCode)
+			then("result list job")
+			assert(null != result)
+			result.length should be (2)
 		}
 	}
 }
