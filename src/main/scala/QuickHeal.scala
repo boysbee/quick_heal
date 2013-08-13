d2863b6e2b2000180627d8dec65ed9ca76ec16af
nattaporn <gogorenger@gmail.com>
2013-08-14 01:59:05 +0700

    revise method find with job and discount.

--- src/main/scala/QuickHeal.scala
+++ src/main/scala/QuickHeal.scala
@@ -1,5 +1,13 @@
 class QuickHeal {
-	def findJobListByDiscountCode(discountCode : String) : List[String] = {
-		return List("CSM1","CSM2")
+	def findJobListByDiscountCode(discountCode : String) : Array[Array[Any]] = {
+		return Array[Array[Any]](List("CSADDCSEGMENT","TTHP-966BGE","DIR040","Thitaree Thongnamsap","Nattaporn Chatmalairut").toArray,
+		Array("CSDCMNPO2R","SNGK-94U6HP", "DIR040","Surisara Ngamtragoonsuk","Nattaporn Chatmalairut").toArray,
+		Array("CSDCMNPO2R","SNGK-94U6HP","DIR042","Surisara Ngamtragoonsuk","Nattaporn Chatmalairut").toArray)
+	}
+
+	def findJobListByJobName(jobName : String) : Array[Array[Any]] = {
+		return Array[Array[Any]](List("CSADDCSEGMENT","TTHP-966BGE","DIR040","Thitaree Thongnamsap","Nattaporn Chatmalairut").toArray,
+		Array("CSDCMNPO2R","SNGK-94U6HP", "DIR040","Surisara Ngamtragoonsuk","Nattaporn Chatmalairut").toArray,
+		Array("CSDCMNPO2R","SNGK-94U6HP","DIR042","Surisara Ngamtragoonsuk","Nattaporn Chatmalairut").toArray)
 	}
 }
