7e73e2e34fb182ebaa19301a854a3f762c1b688d
nattaporn <gogorenger@gmail.com>
2013-08-21 01:59:57 +0700

    use control object for finally close

--- src/main/scala/CsmDiscount.scala
+++ src/main/scala/CsmDiscount.scala
@@ -1,5 +1,14 @@
 case class CsmDiscount(jobName:String)
 
 object CsmDiscount {
-	
+	import db._
+
+	def findDiscount(conn: Connection) : List[CsmDiscount] =
+	using(conn.createStatement) { st=>
+		using(st.executeQuery("select * from csm_discount")) { rs =>
+			bmap(rs.next){
+				new CsmDiscount(rs.getString("job_name"))
+			}
+		}
+	}
 }
