48875b6c5c1659c35523c737f65ac46d7fa3104f
nattaporn <gogorenger@gmail.com>
2013-08-21 02:08:36 +0700

    add first spec

--- src/test/scala/CsmDiscountSpec.scala
+++ src/test/scala/CsmDiscountSpec.scala
@@ -1,7 +1,13 @@
 import org.scalatest._
 
-class CsmDiscountSpec extends FreeSpec {
+class CsmDiscountSpec extends FreeSpec with GivenWhenThen{
 	"A CsmDiscount" - {
-	
+		"It should find with discount code" in {
+			Given("a discount code")
+
+			When("findWithDiscount")
+
+			Then("return list not null")
+		}
 	}
 }
