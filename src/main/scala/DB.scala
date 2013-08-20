8d62f9ae024bed17df8b56e8b42f7e7492aa4b64
nattaporn <gogorenger@gmail.com>
2013-08-21 01:38:29 +0700

    add control object

--- src/main/scala/DB.scala
+++ src/main/scala/DB.scala
@@ -1,6 +1,21 @@
 package db
 
+object Control {
+	def using[A <: { def close(): Unit},B] (param : A) (f: A => B): B = 
+	try {
+		f(param)
+	} finally {
+		param.close()
+	}
 
+	import scala.collection.mutable.ListBuffer
+
+	def bmap[T] ( test : => Boolean)(block : => T) :List[T] = {
+			val ret = new ListBuffer[T]
+			while(test) ret += block
+			ret.toList
+	}
+}
 object DB {
 	def apply(url: String , user : String , pass : String ) : DB = {
 		new DB(url,user,pass)
