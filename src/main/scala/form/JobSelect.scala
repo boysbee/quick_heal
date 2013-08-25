c1492ab3029a1ae01e35d8e0954d124fb17ca529
nattaporn <gogorenger@gmail.com>
2013-08-26 02:17:13 +0700

    update JobSelect

--- src/main/scala/form/JobSelect.scala
+++ src/main/scala/form/JobSelect.scala
@@ -7,18 +7,23 @@ class JobSelect extends BoxPanel(Orientation.Vertical) {
 	peer.setPreferredSize(new Dimension(640, 580))
 	val ui = new BoxPanel(Orientation.Vertical){
 		val findPanel = new FlowPanel {
-			contents += new Label("Fill detail for test : ")
-			contents += new Label("")
-			contents += new Label("Ban:", null , Alignment.Left)
+
+			contents += new Label("Job:", null , Alignment.Left)
 			val banTextField = new TextField("",15)
 			contents += banTextField
-			contents += new Label("Subscriber No :", null , Alignment.Left)
+			contents += new Label("UCR No :", null , Alignment.Left)
 			val subTextField = new TextField("",10)
 			contents += subTextField
+			contents += new Label("Discount Code :", null , Alignment.Left)
+			val discountCodeTextField = new TextField("",10)
+			contents += discountCodeTextField
 			contents += new Button(Action("Find") {
 				println("@@ do find!")
 			});
 		}
+		contents += findPanel
+
+		contents += new TestJob(null)
 		
 	}
 
