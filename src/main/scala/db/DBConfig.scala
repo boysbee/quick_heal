0d0ec113a2709d514215a2170e87784b75c602d9
nattaporn <gogorenger@gmail.com>
2013-08-26 23:57:00 +0700

    update DBConfig

--- src/main/scala/db/DBConfig.scala
+++ src/main/scala/db/DBConfig.scala
@@ -1,3 +1,12 @@
 package db
 
 case class DBConfig(url : String , user : String , pass : String)
+
+object DBConfig {
+	val url = "jdbc:oracle:thin:@172.16.49.14:1521:TEST01"
+	// val url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl"
+	val user = "amdapp22"
+	val pass = "amdapp22"
+	
+	def apply = new DBConfig(url,user,pass)
+}
